b45d85ed7581cdd0ee3ce6a267df781a92da9bc9
Code-Rrrrr <mohammadrayyansyed001@gmail.com>
2026-01-19 23:48:35 +0530

    add: add some ui

---
+++ lib/widgets/daily_goal_ring.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/daily_goal_provider.dart';
+
+class DailyGoalRing extends StatelessWidget {
+  const DailyGoalRing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final goalProvider = context.watch<DailyGoalProvider>();
+    final goal = goalProvider.dailyGoal;
+
+    return Column(
+      children: [
+        SizedBox(
+          width: 140,
+          height: 140,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: goal.progress,
+                strokeWidth: 10,
+                backgroundColor: Colors.grey.shade300,
+                valueColor: AlwaysStoppedAnimation(
+                  goal.progress == 1.0
+                      ? Colors.green
+                      : Theme.of(context).primaryColor,
+                ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '${goal.completed} / ${goal.goal}',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const Text('Today'),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
