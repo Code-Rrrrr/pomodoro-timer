1e5f29bea870406ad27fc051de72d9c03f0a9b51
Code-Rrrrr <mohammadrayyansyed001@gmail.com>
2026-01-20 22:21:15 +0530

    add: add the focus session feature

--- lib/widgets/daily_goal_ring.dart
+++ lib/widgets/daily_goal_ring.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/daily_goal_provider.dart';
 
+
 class DailyGoalRing extends StatelessWidget {
   const DailyGoalRing({super.key});
 
